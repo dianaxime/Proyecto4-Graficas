2c61bd3b4314715c80086d6737506b87f28f8cd6
DianaXimena <dianaxime0@gmail.com>
2020-11-21 17:27:46 -0600

    otro posible shader

--- gl.py
+++ gl.py
@@ -22,11 +22,13 @@ uniform vec3 light;
 out float intensity;
 out vec2 vertexTexcoords;
 out vec3 lPosition;
+out vec3 fNormal;
 
 void main()
 {
 	vertexTexcoords = texcoords;
 	lPosition = position;
+	fNormal = normal;
 	intensity = dot(normal, normalize(light));
 	gl_Position = theMatrix * vec4(position.x, position.y, position.z, 1.0);
 }
@@ -39,6 +41,7 @@ layout(location = 0) out vec4 fragColor;
 in float intensity;
 in vec2 vertexTexcoords;
 in vec3 lPosition;
+in vec3 fNormal;
 
 uniform sampler2D tex;
 uniform vec4 diffuse;
@@ -46,8 +49,7 @@ uniform vec4 ambient;
 
 void main()
 {
-
-	fragColor = vec4(lPosition.x * 5.0, lPosition.y * 8.0, lPosition.z * 5.0, 1.0) * intensity;
+	fragColor = vec4(cross(fNormal, lPosition), 1.0);
 }
 """
 
--- shaders.py
+++ shaders.py
@@ -76,3 +76,46 @@ void main()
 	fragColor = vec4(lPosition.x * 5.0, lPosition.y * 8.0, lPosition.z * 5.0, 1.0) * intensity;
 }
 """
+
+vertex_shader = """
+#version 460
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 texcoords;
+
+uniform mat4 theMatrix;
+uniform vec3 light;
+
+out float intensity;
+out vec2 vertexTexcoords;
+out vec3 lPosition;
+out vec3 fNormal;
+
+void main()
+{
+	vertexTexcoords = texcoords;
+	lPosition = position;
+	fNormal = normal;
+	intensity = dot(normal, normalize(light));
+	gl_Position = theMatrix * vec4(position.x, position.y, position.z, 1.0);
+}
+"""
+
+fragment_shader = """
+#version 460
+layout(location = 0) out vec4 fragColor;
+
+in float intensity;
+in vec2 vertexTexcoords;
+in vec3 lPosition;
+in vec3 fNormal;
+
+uniform sampler2D tex;
+uniform vec4 diffuse;
+uniform vec4 ambient;
+
+void main()
+{
+	fragColor = vec4(cross(fNormal, lPosition), 1.0);
+}
+"""
